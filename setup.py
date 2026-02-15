418f26863b92433b8040363bb43275fb9bd12d74
Wong Hoi Sing Edison <hswong3i@pantarei-design.com>
2026-02-15 15:52:37 +0800

    alvistack/v4.26.0
        git clean -xdf
        tar zcvf ../python-jsonschema_4.26.0.orig.tar.gz --exclude=.git .
        debuild -uc -us
        cp python-jsonschema.spec ../python-jsonschema_4.26.0-1.spec
        cp ../python*-jsonschema*4.26.0*.{gz,xz,spec,dsc} /osc/home\:alvistack/python-jsonschema-jsonschema-4.26.0/
        rm -rf ../python*-jsonschema*4.26.0*.*
    
    Signed-off-by: Wong Hoi Sing Edison <hswong3i@pantarei-design.com>

---
+++ setup.py
@@ -0,0 +1,63 @@
+from setuptools import setup
+
+setup(
+    name='jsonschema',
+    version='4.26.0',
+    description='An implementation of JSON Schema validation for Python',
+    author_email='Julian Berman <Julian+jsonschema@GrayVines.com>',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
+        'Programming Language :: Python :: 3.14',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: File Formats :: JSON',
+        'Topic :: File Formats :: JSON :: JSON Schema',
+    ],
+    install_requires=[
+        'attrs>=22.2.0',
+        'jsonschema-specifications>=2023.03.6',
+        'referencing>=0.28.4',
+        'rpds-py>=0.25.0',
+    ],
+    extras_require={
+        'format': [
+            'fqdn',
+            'idna',
+            'isoduration',
+            'jsonpointer>1.13',
+            'rfc3339-validator',
+            'rfc3987',
+            'uri-template',
+            'webcolors>=1.11',
+        ],
+        'format-nongpl': [
+            'fqdn',
+            'idna',
+            'isoduration',
+            'jsonpointer>1.13',
+            'rfc3339-validator',
+            'rfc3986-validator>0.1.0',
+            'rfc3987-syntax>=1.1.0',
+            'uri-template',
+            'webcolors>=24.6.0',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'jsonschema = jsonschema.cli:main',
+        ],
+    },
+    packages=[
+        'jsonschema',
+        'jsonschema.benchmarks',
+        'jsonschema.tests',
+        'jsonschema.tests.typing',
+    ],
+)
